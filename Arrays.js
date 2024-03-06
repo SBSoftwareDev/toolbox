07028953f6470ab224f8d5de0d90d90fb7652352
Sean <michaelbrown.sean@gmail.com>
2024-03-05 22:34:02 -0800

    added Array snippet

---
+++ Arrays.js
@@ -0,0 +1,29 @@
+const names = ["Jack", "Jill", "Tommy", "Alfonso", "Xander", "Mordecai"];
+const numbers = [2, 5, 8, 15, 34, 65, 395, 4996];
+
+/*
+    Quick Reference:
+
+    Map: Create (and return) a new array by doing something with each item.
+    
+    Filter: Create a new array by keeping the items that return true.
+
+    Reduce: Accumulate a value by doing something to each item in an array.
+
+    Find: Find the first matching item in an array.
+
+    FindIndex: Find the index of the first matching item in an array.
+
+*/
+
+// MAP
+
+const squaredNumbers = numbers.map((x) => {
+    return x * x;
+});
+
+// FILTER
+
+const namesWithA = names.filter((name) => {
+    return name.inde
+});
