3440d56e73fc3a1f498041f9aef39294f1863c2b
Sean <michaelbrown.sean@gmail.com>
2024-03-08 12:40:06 -0800

    added more detail to array snippet

--- snippets/Arrays.js
+++ snippets/Arrays.js
@@ -5,15 +5,19 @@ const numbers = [2, 5, 8, 15, 34, 65, 395, 4996];
     Quick Reference:
 
     Map: Create (and return) a new array by doing something with each item.
-    
+        (Element, Index, Array)
+
     Filter: Create a new array by keeping the items that return true.
+        (Element, Index, Array)
 
     Reduce: Accumulate a value by doing something to each item in an array.
+        (Accumulator, CurrentElement, CurrentIndex, Array)
 
     Find: Find the first matching item in an array.
+        (Element, Index, Array)
 
     FindIndex: Find the index of the first matching item in an array.
-
+        (Element, Index, Array)
 */
 
 // MAP
